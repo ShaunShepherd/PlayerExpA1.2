9994696d57c865c2ac96f9eb8d5d4b8e9bec55b7
ShaunShepherd <shaun.shepherd142@gmail.com>
2023-07-26 23:19:46 +1000

    Implement Pufferfish/Add model

--- PlayerExperienceA1/Assets/Scripts/Grow.cs
+++ PlayerExperienceA1/Assets/Scripts/Grow.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Grow : MonoBehaviour, IInteractable
 {
     [SerializeField] float growRate;
     [SerializeField] float maxSize;
+    [SerializeField] float shrinkDelay;
     [SerializeField] ParticleSystem popParticles;
+
+    bool growing;
+    float previousScale;
+    float startingScale;
+    float shrinkDelayTimer;
+
+    void Start()
+    {
+        startingScale = transform.transform.localScale.x;
+    }
+
     public void Interact()
     {
         if (transform.localScale.x < maxSize)
@@ -19,6 +32,44 @@ public class Grow : MonoBehaviour, IInteractable
             particles.transform.parent = null;
             Destroy(gameObject);
         }
+    }
 
+    void Update()
+    {
+
+
+        if (transform.localScale.x > previousScale)
+        {
+            growing = true;
+
+            previousScale = transform.localScale.x;
+
+            shrinkDelayTimer = 0;
+        }
+        else
+        {
+            growing = false;
+            previousScale = transform.localScale.x;
+
+            shrinkDelayTimer += Time.deltaTime;
+
+            if (shrinkDelayTimer > shrinkDelay)
+            {
+                Shrink();
+            }
+        }
+
+
+    }
+
+    void Shrink()
+    {
+        if (!growing)
+        {
+            if (transform.localScale.x > startingScale)
+            {
+                transform.localScale /= 1 + growRate / 1000;
+            }
+        }
     }
 }
