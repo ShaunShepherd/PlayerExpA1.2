038c97e806c021baca973ba62885dba2030cbd6a
hfraser2 <hfraser1219@gmail.com>
2023-08-11 03:37:29 +1000

    Nothing

---
+++ PlayerExperienceA1/Assets/Scripts/Menu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public bool gamePaused;
+    [SerializeField] GameObject menuOverlay;
+
+    void Start()
+    {
+        menuOverlay.SetActive(false);
+        gamePaused = false;
+
+        Time.timeScale = 1;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            if (!gamePaused)
+            {
+                Time.timeScale = 0;
+                menuOverlay.SetActive(true);
+
+                gamePaused = true;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                menuOverlay.SetActive(false);
+
+                gamePaused = false;
+            }
+        }
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1;
+        menuOverlay.SetActive(false);
+        gamePaused = false;
+    }
+
+    public void RestartGame()
+    {
+        Scene scene = SceneManager.GetActiveScene(); 
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
