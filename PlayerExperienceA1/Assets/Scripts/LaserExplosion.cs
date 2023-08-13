a59c733023c110c256704306d0227353757fbf53
ShaunShepherd <140130848+ShaunShepherd@users.noreply.github.com>
2023-08-13 15:20:42 +1000

    Spolison partilces

--- PlayerExperienceA1/Assets/Scripts/LaserExplosion.cs
+++ PlayerExperienceA1/Assets/Scripts/LaserExplosion.cs
@@ -6,13 +6,39 @@ public class LaserExplosion : MonoBehaviour, IInteractable
 {
     [SerializeField] float explosionRadius = 5;
     [SerializeField] float explosionForce = 500;
+    [SerializeField] float laserMoveSpeed;
 
     [SerializeField] GameObject uiText;
     [SerializeField] Transform explosionSpawn;
+    [SerializeField] Transform laserOrigin;
+    [SerializeField] Transform laserEnd;
+    [SerializeField] GameObject particleSpawner;
+    [SerializeField] GameObject initalPop;
+    [SerializeField] GameObject smokeParticle;
+
+    [SerializeField] GameObject trail;
+    
+
+    bool fired = false;
+    bool canFire = true;
+
+    GameObject trailGO;
+
+    Vector3 targetVector;
 
     public void Interact()
     {
-        SpawnExplosion(explosionSpawn.position);
+        if (!fired && canFire)
+        {
+            particleSpawner.SetActive(true);
+
+            trailGO = Instantiate(trail, particleSpawner.transform);
+
+            targetVector = laserEnd.position - laserOrigin.position;
+
+            canFire = false;
+            fired = true;
+        }
     }
 
     public void LookAt()
@@ -22,7 +48,35 @@ public class LaserExplosion : MonoBehaviour, IInteractable
 
     void Start()
     {
+        particleSpawner.SetActive(false);
 
+        particleSpawner.transform.position = laserOrigin.position;
+    }
+
+    void Update()
+    {
+        if (fired && !canFire)
+        {
+            MoveSpawner();
+
+            if (Vector3.Distance(particleSpawner.transform.position, laserEnd.position) < 0.1)
+            {
+                Destroy(trailGO);
+
+                ParticleSpawn(initalPop);
+                ParticleSpawn(smokeParticle);
+
+                particleSpawner.SetActive(false);
+
+                particleSpawner.transform.position = laserOrigin.position;
+
+                SpawnExplosion(explosionSpawn.position);
+
+                canFire = true;
+                fired = false;
+
+            }
+        }
     }
 
 
@@ -62,5 +116,22 @@ public class LaserExplosion : MonoBehaviour, IInteractable
     {
         Gizmos.color = Color.cyan;
         Gizmos.DrawSphere(explosionSpawn.position, explosionRadius);
+    }
+
+    void MoveSpawner()
+    {
+        if (Vector3.Distance(trailGO.transform.position, particleSpawner.transform.position) < 0.1)
+        {
+            trailGO.transform.position = laserEnd.position;
+        }
+
+        particleSpawner.transform.Translate(targetVector.normalized * Time.deltaTime * laserMoveSpeed);
+    }
+
+    void ParticleSpawn(GameObject go)
+    {
+        var initalPopPar = Instantiate(go, particleSpawner.transform);
+        initalPopPar.transform.parent = null;
+
     }
 }
